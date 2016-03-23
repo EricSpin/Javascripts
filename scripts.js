5dfc66e90813b005b3b95a5f00eb31f66b152b4e
Erick Suarez <ssping5@gmail.com>
2016-03-23 10:20:24 -0500

    Creacion de Cuento en JS

--- scripts.js
+++ scripts.js
@@ -1,4 +1,35 @@
+/*console.log("Inicio de programa")
 alert("HOLA JAVASCRIPTS.0");
-document.write("<h1>Hola JavaScript!</h1>");
+document.write("<h1>Hola JS UPSE!</h1>");
+alert("Gracias por Visitar");
+alert("Mira");
+console.log("Fin de programa")*/
 
-alert("HOLA JAVASCRIPTS.0");
+
+/*console.log("Inicio de Programa");
+var equipo = prompt("Cual es tu equipo de futbol favorito?");
+document.write("<h1>El Equipo es!</h1>" + equipo);
+console.log("El Equipo es:" + equipo);
+console.log("Fin de programa");*/
+
+/*console.log("Inicio de Programa");
+var clave = prompt("Hola usuario, Ingrese su Clave!");
+document.write("<h1>clave!</h1>" + clave.length);
+document.write("<h1>clave!</h1>" + clave)
+console.log("la Longitud de la clave:" + clave.length);
+console.log("TA-DA!!!Su clave es:" +  clave.toUpperCase());
+console.log("El Valor original de la clave es:"+ clave);
+console.log("Fin de programa");*/
+
+console.log("Inicio del Programa");
+var frase1 = prompt("Hola usuario, Ingrese Sustantivo!");
+var frase2 = prompt("Hola usuario, Ingrese adjetivo!");
+var frase3 = prompt("Hola usuario, Ingrese verbo!");
+document.write("<h1>CUENTO!</h1>");
+document.write("<h2>El " + frase1 +" de razas "  + frase2 +" pueden vivir "+ frase3+"</h2> ");
+document.write("<h2>Feroses " + frase2 + " el " +  frase1 +" pueden vivir " + frase3+"</h2> ");
+document.write("<h2>pueden vivir " + frase3 +" de razas " + frase2 +" El "+ frase1+" </h2>");
+/*console.log("la Longitud de la clave:" + clave.length);
+console.log("TA-DA!!!Su clave es:" +  clave.toUpperCase());
+console.log("El Valor original de la clave es:"+ clave);*/
+console.log("Fin de programa");
