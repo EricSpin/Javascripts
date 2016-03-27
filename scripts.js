06d00a6aa3084434693aa6117f7d2ad6aa2e26c2
Erick Suarez <ssping5@gmail.com>
2016-03-27 09:56:12 -0500

    cambio de cuenta cuentos

--- scripts.js
+++ scripts.js
@@ -21,8 +21,24 @@ console.log("TA-DA!!!Su clave es:" +  clave.toUpperCase());
 console.log("El Valor original de la clave es:"+ clave);
 console.log("Fin de programa");*/
 
-/*console.log("Inicio del Programa");
-var frase1 = prompt("Hola usuario, Ingrese Sustantivo!");
+console.log("Inicio del Programa");
+var preguntas = 3;
+var preguntasFaltantes = "Quedan" + preguntas + "preguntas.";
+alert("Este es el juego del cuenta cuentos. Sigue las instrucciones.");
+var sustantivo1 = prompt("Ingrese un Sustantivo."+preguntasFaltantes);
+preguntas = preguntas - 1;
+preguntasFaltantes = "Quedan" + preguntas + "preguntas.";
+var adjetivo1 = prompt("Ingrese un Adjetivo."+preguntasFaltantes);
+preguntas -= 1;
+preguntasFaltantes = "Quedan" + preguntas + "preguntas.";
+var verbo1 = prompt("Ingrese un Verbo."+preguntasFaltantes);
+var cuento = "El" + adjetivo1 + " " + sustantivo1 + "queria" + verbo1 + "";
+document.write("<h1>Cuento</h1><p>" + cuento + "</p>");
+console.log("Programa Completo");
+
+
+
+/*var frase1 = prompt("Hola usuario, Ingrese Sustantivo!");
 var frase2 = prompt("Hola usuario, Ingrese adjetivo!");
 var frase3 = prompt("Hola usuario, Ingrese verbo!");
 document.write("<h1>CUENTO!</h1>");
@@ -33,26 +49,3 @@ document.write("<h2>pueden vivir " + frase3 +" de razas " + frase2 +" El "+ fras
 console.log("TA-DA!!!Su clave es:" +  clave.toUpperCase());
 console.log("El Valor original de la clave es:"+ clave);*/
 /*console.log("Fin de programa");*/
-
-/*var segundosPorMinuto = 60;
-var minutosPorHora = 60;
-var horasPorDia = 24;
-var segundosPorDia = segundosPorMinuto * minutosPorHora * horasPorDia;
-document.write("<h1>Calculadora del Tiempo</h1><p>Hay" + segundosPorDia + "segundos en un dia</p>");*/
-
-/*var segundosPorMinuto = 60;
-var minutosPorHora = 60;
-var horasPorDia = 24;
-var diasPorMes = 30;
-var mesPorAño = 12;
-var segundosPorDia = segundosPorMinuto * minutosPorHora * horasPorDia;
-document.write("<h1>Calculadora del Tiempo</h1><p>Hay" + segundosPorDia + "segundos en un dia</p>");
-var Año = segundosPorMinuto * minutosPorHora * horasPorDia * diasPorMes * mesPorAño;
-document.write("<h1>Años de Vida</h1><p>Hay" + Año + "dias total</p>")*/
-
-console.log("Inicio del Programa");
-var peliculasVistas = prompt("Cuantas peliculas has visto este mes?");
-var seriesVistas = prompt("Cuantas series has visto este mes?");
-var tiempoPantalla = parseInt(peliculasVistas)+parseInt(seriesVistas);
-alert("Wow! Has visto" + tiempoPantalla + "veces minimo la pantalla.");
-console.log("Fin de programa");
